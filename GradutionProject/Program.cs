ecda5e31e8f6e0f400f9ad1ba4dcba12b68dc557
beilin git <MY_NAME@example.com>
2024-10-08 22:51:43 +0300

    Add Services To program.cs And Add MudBlazor

--- GradutionProject/Program.cs
+++ GradutionProject/Program.cs
@@ -1,10 +1,29 @@
 using GradutionProject.Components;
-
+using GradutionProject.Data;
+using GradutionProject.Interfaces;
+using GradutionProject.Repositories;
+using Microsoft.EntityFrameworkCore;
+using MudBlazor.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+
+var desktopInfo = System.Net.Dns.GetHostName();
+var connectionString = $"Server={desktopInfo}\\MSSQLSERVERTPS;Database=Gra;User Id=sa; Password=sas;Trusted_Connection=false; MultipleActiveResultSets=true;Encrypt=no";
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString),
+    ServiceLifetime.Transient, ServiceLifetime.Transient
+);
+
+
+
+//Add Services
+builder.Services.AddMudServices();
+builder.Services.AddScoped<IAuthentication, AuthenticationRepository>();
+builder.Services.AddScoped<UserService>();
 
 var app = builder.Build();
 
